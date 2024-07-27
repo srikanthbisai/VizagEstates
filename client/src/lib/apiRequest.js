63431f964b12e0edac2f6112929079e97fca06c0
Srikanth Bisai <94598179+srikanthbisai@users.noreply.github.com>
2024-07-27 22:16:22 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "https://vizagestates-1.onrender.com/api",
+    baseURL: "http://localhost:8000/api",
    withCredentials: true,
 });
 
